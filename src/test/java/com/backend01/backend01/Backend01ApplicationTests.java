494cccd3de98daa93f921b80cf020442fe294bd0
Cesar Herrera <cesarhhm1998@gmail.com>
2025-10-30 13:53:52 -0500

    Backend01

--- src/test/java/com/backend01/backend01/Backend01ApplicationTests.java
+++ src/test/java/com/backend01/backend01/Backend01ApplicationTests.java
@@ -1,10 +1,10 @@
-package com.backen01.backen01;
+package com.backend01.backend01;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Backen01ApplicationTests {
+class Backend01ApplicationTests {
 
 	@Test
 	void contextLoads() {
