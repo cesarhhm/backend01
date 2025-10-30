111200e4aa904040561a6384844c52333f318f09
Cesar Herrera <cesarhhm1998@gmail.com>
2025-10-30 18:53:50 -0500

    Corrijo apiv2

--- src/main/java/com/backend01/backend01/config/WebConfig.java
+++ src/main/java/com/backend01/backend01/config/WebConfig.java
@@ -1,30 +1,31 @@
-package com.backend01.backend01.config;
+//package com.backend01.backend01.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-public class WebConfig {
-	
-	  @Bean
-	    public WebMvcConfigurer corsConfigurer() {
-	        return new WebMvcConfigurer() {
-	            @Override
-	            public void addCorsMappings(CorsRegistry registry) {
-	                registry.addMapping("/**")
-	                        // ⚠️ Cambia por tu dominio real del frontend en Azure
-	                        .allowedOrigins(
-	                            "https://api-rest-helpdesk-fqaff3axb8e0deep.canadacentral-01.azurewebsites.net",
-	                            "http://localhost:5173"
-	                        )
-	                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-	                        .allowedHeaders("*")
-	                        .allowCredentials(false);
-	            }
-	        };
-	    }
-	}
+//@Configuration
+//public class WebConfig {
+//	
+//	  @Bean
+//	    public WebMvcConfigurer corsConfigurer() {
+//	        return new WebMvcConfigurer() {
+//	            @Override
+//	            public void addCorsMappings(CorsRegistry registry) {
+//	                registry.addMapping("/**")
+//	                        // ⚠️ Cambia por tu dominio real del frontend en Azure
+//	                        .allowedOrigins(
+//	                            "https://app-web-frontend-dhb4f3h3drfzcndy.canadacentral-01.azurewebsites.net",
+//	                            "http://localhost:5173",
+//	                            "http://localhost:3001"
+//	                        )
+//	                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+//	                        .allowedHeaders("*")
+//	                        .allowCredentials(false);
+//	            }
+//	        };
+//	    }
+//	}
 
 
--- src/main/java/com/backend01/backend01/rest/TicketRest.java
+++ src/main/java/com/backend01/backend01/rest/TicketRest.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 @RequestMapping("/ticket")
-
+@CrossOrigin(origins = "http://localhost:3001")
 public class TicketRest {
 
 	@Autowired
